0ed8f8b3fd18f51a7ffeebe798b28974ef246fe4
Obinna Uche <75355071+ObiUche@users.noreply.github.com>
2026-02-23 19:06:44 +0000

    Added GETMAPPING Pathvaribale

--- src/main/java/com/example/task/controller/TaskController.java
+++ src/main/java/com/example/task/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.example.task.controller;
 
 import com.example.task.entity.Task;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,11 +12,17 @@ public class TaskController {
 
     private final List<Task> taskList = List.of(
             new Task(1, "Task1", false, "A first test task"),
-            new Task(2, "Task2", false, "A first test task")
+            new Task(2, "Task2", false, "A Second test task")
     );
 
+    // Return all tasks
     @GetMapping("/tasks")
     public List<Task> getTasks(){
         return taskList;
+    }
+
+    @GetMapping("/tasks/{id}")
+    public Task getTaskById(@PathVariable int id){
+        return taskList.get(id - 1);
     }
 }
